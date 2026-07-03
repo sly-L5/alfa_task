956a1bf3aad7c8d255bc70290593463950e46f47
sly-L5 <Leo.Sark@yandex.ru>
2026-07-03 18:10:59 +0300

    Update Main.java
    Исправленный код, задание2

--- task_2/Random/src/Main.java
+++ task_2/Random/src/Main.java
@@ -1,27 +1,23 @@
 import java.util.Random;
 
-
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
-
 
         int age = random.nextInt(101);
         double money = random.nextDouble(500_000);
         boolean blackList = false;
         boolean invitation = true;
-        double vznos = (float) (money * 0.925);
-        vznos = money - vznos;
-        if (age >= 18 && (money >= 50_000 || invitation == true) && blackList == false) {
+        double vznos = money * 0.075;
 
+        if (age >= 18 && (money >= 50_000 || invitation) && !blackList) {
             System.out.println("Добро пожаловать!");
-
-        }else {
+        } else {
             System.out.println("Вы не подходите(");
         }
+
         System.out.println("Возраст: " + age);
         System.out.println("Деньги: " + money);
         System.out.println("Добровольный взнос: " + vznos);
-
     }
 }

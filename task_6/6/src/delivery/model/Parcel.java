68b898b235a69941512f7e10ef2dbeb2b0508ea2
sly-L5 <Leo.Sark@yandex.ru>
2026-07-14 14:43:35 +0300

    Update Parcel.java

--- task_6/6/src/delivery/model/Parcel.java
+++ task_6/6/src/delivery/model/Parcel.java
@@ -4,6 +4,28 @@ public class Parcel {
     private String recipientName;
     private String deliveryAddress;
     protected double weight;
+    String trackingNumber;
+
+    public Parcel(String recipientName, String deliveryAddress, double weight, String trackingNumber) {
+        this.recipientName = recipientName;
+        this.deliveryAddress = deliveryAddress;
+        this.weight = weight;
+        this.trackingNumber = trackingNumber;
+    }
+
+    public Parcel() {
+        this.recipientName = "DEFAULT";
+        this.deliveryAddress = "DEFAULT";
+        this.weight = 0.0;
+        this.trackingNumber = "000000";
+    }
+
+ package delivery.model;
+
+public class Parcel {
+    private String recipientName;
+    private String deliveryAddress;
+    protected double weight;
     private String trackingNumber;
 
     public Parcel(String recipientName, String deliveryAddress, double weight, String trackingNumber) {
@@ -20,8 +42,40 @@ public class Parcel {
         this.trackingNumber = "000000";
     }
 
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
     public double calculateDeliveryPrice() {
-        return weight * 50;
+        return 100 + weight * 30;
     }
 
     public void printInfo() {
